d290531f090c22b2b387020fdc3680a4ff9df9ad
SEAF39 <labber@DESKTOP-7J1C764>
2023-05-01 16:40:30 -0400

    update the seeds - fix issues

--- db/seeds/03_car_wishlist.sql
+++
@@ -1,7 +0,0 @@
-INSERT INTO car_wishlist (wishlist_id, cars_id)
-VALUES
-    (1, 2),
-    (1, 3),
-    (2, 1),
-    (3, 4),
-    (4, 5);
--- db/seeds/06_cars_wishlist.sql
+++
@@ -1,6 +0,0 @@
-INSERT INTO car_wishlist (wishlist_id, cars_id)
-VALUES
-  (1, 2),
-  (2, 1),
-  (3, 3),
-  (4, 2);
