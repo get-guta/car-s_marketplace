1e79d8bd37d6cb24b07c097204a835c2999333e7
getenesh <getenesh.guta@gmail.com>
2023-05-03 18:07:58 -0400

    login feature and wishlist creation

--- public/scripts/cars.js
+++ public/scripts/cars.js
@@ -1,4 +1,74 @@
-$(() => {
+
+function createTimeElampled(date_created) {
+  let currentDate = new Date();
+  let createdDate = new Date(date_created);
+  let diffInMs = currentDate - createdDate;
+
+  let diffInMins = Math.floor(diffInMs / (1000 * 60));
+  let diffInHours = Math.floor(diffInMs / (1000 * 60 * 60));
+  let diffInDays = Math.floor(diffInMs / (1000 * 60 * 60 * 24));
+
+  let timeElapsed;
+
+  if (diffInMins < 60) {
+    timeElapsed = `${diffInMins} minute${diffInMins > 1 ? 's' : ''} ago`;
+  } else if (diffInHours < 24) {
+    timeElapsed = `${diffInHours} hour${diffInHours > 1 ? 's' : ''} ago`;
+  } else {
+    timeElapsed = `${diffInDays} day${diffInDays > 1 ? 's' : ''} ago`;
+  }
+  return timeElapsed;
+}
+// get userid session value from hidden input
+let userId = $('#userid').val();
+
+function createCarCard(car) {
+  const timeElapsed = createTimeElampled(car.date_created);
+  const $card = $('<div>').addClass('card mb-3').css('max-width', '540px');
+  const $row = $('<div>').addClass('row g-0').appendTo($card);
+  const $col1 = $('<div>').addClass('col-md-4').appendTo($row);
+  $('<img>').attr({
+    src: `../assets/${car.image}`,
+    alt: car.model,
+    width: 200,
+    height: 200,
+  }).addClass('img-fluid rounded-start').appendTo($col1);
+  const $col2 = $('<div>').addClass('col-md-8').appendTo($row);
+  const $cardBody = $('<div>').addClass('card-body').appendTo($col2);
+  $('<h5>').addClass('card-title').text(`${car.model} - ${car.price}`).appendTo($cardBody);
+  $('<p>').addClass('card-text').text(car.description).appendTo($cardBody);
+  $('<p>').addClass('card-text').append($('<small>').addClass('text-muted').text(timeElapsed)).appendTo($cardBody);
+  const $favoriteIcon = $('<a>').attr('id', `favorite-${car.id}`);
+  const $icon = $('<i>').addClass('fa-regular fa-heart');
+  $favoriteIcon.append($icon);
+  $favoriteIcon.click(function () {
+    // Make an AJAX request to insert the car_id into the cars table
+    let carId = car.id;
+
+    console.log("client", userId, carId);
+    $.ajax({
+      method: 'POST',
+      url: 'api/wishlist',
+      data: {
+        car_id: carId,
+        user_id: userId
+      },
+      success: function () {
+        // Change the color of the favorite icon button to red
+        $icon.removeClass('fa-regular fa-heart').addClass('fa-solid fa-heart').css('color', '#ff3300');
+      },
+    });
+  });
+  // Append the favorite icon button to the car details
+  $cardBody.append($favoriteIcon);
+
+  return $card;
+}
+/// end of createCarCard functions
+
+
+
+$(document).ready(function () {
   //on page load
   $.ajax({
     method: 'GET',
@@ -7,25 +77,9 @@ $(() => {
     .done((response) => {
       const $carsList = $('#cars');
       $carsList.empty();
-
       for (const car of response.cars) {
-        let cardetail = `
-        <div class="card mb-3" style="max-width: 540px;">
-        <div class="row g-0">
-    <div class="col-md-4">
-      <img src="../assets/${car.image}" alt="${car.model}" width="200" height="200" class="img-fluid rounded-start">
-    </div>
-    <div class="col-md-8">
-      <div class="card-body">
-        <h5 class="card-title">${car.model} - ${car.price}</h5>
-        <p class="card-text">${car.description}</p>
-        <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
-      </div>
-    </div>
-  </div>
-  </div>
-          `;
-        $("#cars").append(cardetail);
+        const $carCard = createCarCard(car);
+        $('#cars').append($carCard);
       };
     });
 
@@ -49,23 +103,9 @@ $(() => {
         $carsList.empty();
 
         for (const car of response.cars) {
-          let cardetail = `
-        <div class="card mb-3" style="max-width: 540px;">
-        <div class="row g-0">
-    <div class="col-md-4">
-      <img src="../assets/${car.image}" alt="${car.model}" width="200" height="200" class="img-fluid rounded-start">
-    </div>
-    <div class="col-md-8">
-      <div class="card-body">
-        <h5 class="card-title">${car.model} - ${car.price}</h5>
-        <p class="card-text">${car.description}</p>
-        <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
-      </div>
-    </div>
-  </div>
-  </div>
-          `;
-          $("#cars").append(cardetail);
+          const $carCard = createCarCard(car);
+          $('#cars').append($carCard);
+
         }
       });
   });
--- server.js
+++ server.js
@@ -5,6 +5,8 @@ require('dotenv').config();
 const sassMiddleware = require('./lib/sass-middleware');
 const express = require('express');
 const morgan = require('morgan');
+const cookieSession = require('cookie-session');
+const db = require('./db/connection');
 
 const PORT = process.env.PORT || 8080;
 const app = express();
@@ -14,7 +16,12 @@ app.set('view engine', 'ejs');
 // Load the logger first so all (static) HTTP requests are logged to STDOUT
 // 'dev' = Concise output colored by response status for development use.
 //         The :status token will be colored red for server error codes, yellow for client error codes, cyan for redirection codes, and uncolored for all other codes.
+
 app.use(morgan('dev'));
+app.use(cookieSession({
+  name: 'session',
+  keys: ['key1', 'key2']
+}))
 app.use(express.urlencoded({ extended: true }));
 app.use(
   '/styles',
@@ -35,22 +42,36 @@ app.use(express.static('public'));
 const userApiRoutes = require('./routes/users-api');
 const carApiRoutes = require('./routes/cars-api');
 const priceApiRoutes = require('./routes/price-api');
+const wishlistApiRoutes = require('./routes/wishlist-api');
+
 
 app.use('/api/users', userApiRoutes);
 app.use('/api/cars', carApiRoutes);
 app.use('/api/price-filter', priceApiRoutes);
+app.use('/api/wishlist', wishlistApiRoutes);
 
 //page routes
 const carsRoutes = require('./routes/cars');
 const usersRoutes = require('./routes/users');
+const loginRoutes = require('./routes/login');
 
 app.use('/cars', carsRoutes);
 app.use('/users', usersRoutes);
+app.use('/login', loginRoutes);
 
 app.get('/', (req, res) => {
-  res.render('cars');
-});
+  const user_id = req.session.user_id;
+  db.query('SELECT * FROM users WHERE id = $1', [user_id])
+  .then(data => {
+    const userinfo = data.rows[0];
+    console.log(userinfo);
+    res.render('cars',{userinfo});
+  })
+  .catch(error => {
+    console.log(error.message);
+  })
 
+});
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}`);
